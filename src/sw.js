5c6b7cd25bfb542ff4dbeca419952b5581043bcd
Aarushi <aarushi@iimjobs.com>
2020-08-04 00:12:55 +0530

    Comment sw

--- src/sw.js
+++ src/sw.js
@@ -1,25 +1,25 @@
-var CACHE_NAME = 'my-cache-v1';
-var urlsToCache = [
-  '/',
-  '/styles/styles.css',
-  '/script/webpack-bundle.js'
-];
+// var CACHE_NAME = 'my-cache-v1';
+// var urlsToCache = [
+//   '/',
+//   '/styles/styles.css',
+//   '/script/webpack-bundle.js'
+// ];
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        // Open a cache and cache our files
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+// self.addEventListener('install', function(event) {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(function(cache) {
+//         // Open a cache and cache our files
+//         return cache.addAll(urlsToCache);
+//       })
+//   );
+// });
 
-self.addEventListener('fetch', function(event) {
-  console.log(event.request.url);
-  event.respondWith(
-      caches.match(event.request).then(function(response) {
-          return response || fetch(event.request);
-      })
-  );
-});
+// self.addEventListener('fetch', function(event) {
+//   console.log(event.request.url);
+//   event.respondWith(
+//       caches.match(event.request).then(function(response) {
+//           return response || fetch(event.request);
+//       })
+//   );
+// });
